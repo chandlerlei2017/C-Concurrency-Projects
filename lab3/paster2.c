a60f810d8ed045b927a633c079d551a8f7464c0a
chandlerlei2017 <chandlerlei87@gmail.com>
2019-06-26 23:24:32 -0400

    producer updates

--- lab3/paster2.c
+++ lab3/paster2.c
@@ -39,10 +39,16 @@
 #include "zutil.h"
 #include "crc.h"
 
-#define IMG_URL "http://ece252-1.uwaterloo.ca:2530/image?img=1&part=20"
+#define IMG_URL "http://ece252-1.uwaterloo.ca:2530/image?img=1"
 #define DUM_URL "https://example.com/"
 #define ECE252_HEADER "X-Ece252-Fragment: "
-#define BUF_SIZE 10240 /* 1024*10 = 10K */
+#define BUF_SIZE 1048576  /* 1024*1024 = 1M */
+#define BUF_INC  524288   /* 1024*512  = 0.5M */
+
+#define max(a, b) \
+   ({ __typeof__ (a) _a = (a); \
+       __typeof__ (b) _b = (b); \
+     _a > _b ? _a : _b; })
 
 /* This is a flattened structure, buf points to
    the memory address immediately after
@@ -68,7 +74,7 @@
    + buf[max_size-1]| 1 byte
    +================+
 */
-typedef struct recv_buf_flat {
+typedef struct recv_buf2 {
     char *buf;       /* memory to hold a copy of received data */
     size_t size;     /* size of valid data in buf in bytes*/
     size_t max_size; /* max capacity of buf in bytes*/
@@ -81,7 +87,7 @@ typedef struct recv_chunk {
     size_t size;
 	size_t max_size;
     int seq;
-	
+
 } recv_chunk;
 
 typedef struct queue {
@@ -94,19 +100,19 @@ typedef struct queue {
 
 typedef struct g_vars {
 	int num_pics;
+    int image_num;
 	pthread_mutex_t count_mutex;
 	pthread_mutex_t queue_mutex;
 } g_vars;
 
-
-
 size_t header_cb_curl(char *p_recv, size_t size, size_t nmemb, void *userdata);
-size_t write_cb_curl(char *p_recv, size_t size, size_t nmemb, void *p_userdata);
+size_t write_cb_curl3(char *p_recv, size_t size, size_t nmemb, void *p_userdata);
 int recv_buf_init(RECV_BUF *ptr, size_t max_size);
-int recv_buf_cleanup(recv_chunk *ptr);
-int write_file(const char *path, const void *in, size_t len);
+int recv_buf_cleanup(RECV_BUF *ptr);
+int write_file(int seq, const void *in, size_t len, int* count);
+
 void queue_init(queue* q, void* start, int buf_size);
-void global_init(g_vars* g);
+void global_init(g_vars* g, int img);
 void producer(queue* q, g_vars* g);
 
 void queue_init(queue* q, void* start, int buf_size) {
@@ -124,118 +130,43 @@ void queue_init(queue* q, void* start, int buf_size) {
     }
 }
 
-void global_init(g_vars* g) {
+void global_init(g_vars* g, int img) {
 	g -> num_pics = 0;
+    g -> image_num = img;
 	pthread_mutex_init(&(g -> count_mutex), NULL);
 	pthread_mutex_init(&(g -> queue_mutex), NULL);
 }
 
-void producer(queue* q, g_vars* g) {
-	CURL *curl_handle;
-	CURLcode res;
-	char url[256];
-	recv_chunk img_chunk;
-		
-	while(1){
-		shm_recv_buf_init(&img_chunk, BUF_SIZE);
-		pthread_mutex_lock(&(g -> count_mutex));
-		int imageNum = g -> num_pics;
-		
-		if (imageNum == 50) {
-			pthread_mutex_unlock(&(g -> count_mutex));
-			break;
-		}
-		(g -> num_pics)++;
-		pthread_mutex_unlock(&(g -> count_mutex));	
-		
-		curl_global_init(CURL_GLOBAL_DEFAULT);
-		curl_handle = curl_easy_init();
-		
-		if (curl_handle == NULL) {
-          fprintf(stderr, "curl_easy_init: returned NULL\n");
-		}
-		
-		/* specify URL to get */
-	    curl_easy_setopt(curl_handle, CURLOPT_URL, url);
-
-	    /* register write call back function to process received data */
-	    curl_easy_setopt(curl_handle, CURLOPT_WRITEFUNCTION, write_cb_curl);
-	    /* user defined data structure passed to the call back function */
-	    curl_easy_setopt(curl_handle, CURLOPT_WRITEDATA, (void *)&img_chunk);
-
-	    /* register header call back function to process received header data */
-	    curl_easy_setopt(curl_handle, CURLOPT_HEADERFUNCTION, header_cb_curl);
-	    /* user defined data structure passed to the call back function */
-	    curl_easy_setopt(curl_handle, CURLOPT_HEADERDATA, (void *)&img_chunk);
-
-	    /* some servers requires a user-agent field */
-	    curl_easy_setopt(curl_handle, CURLOPT_USERAGENT, "libcurl-agent/1.0");
-
-
-	    /* get it! */
-	    res = curl_easy_perform(curl_handle);
-
-	    if( res != CURLE_OK) {
-		  fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
-	    }
-
-	    //write_file(img_chunk.seq, img_chunk.buf, img_chunk.size, p_in -> count);
-
-	    /* cleaning up */
-	    curl_easy_cleanup(curl_handle);
-    	curl_global_cleanup();
-	    recv_buf_cleanup(&img_chunk);
-	}
-}
-
-/**
- * @brief  cURL header call back function to extract image sequence number from
- *         http header data. An example header for image part n (assume n = 2) is:
- *         X-Ece252-Fragment: 2
- * @param  char *p_recv: header data delivered by cURL
- * @param  size_t size size of each memb
- * @param  size_t nmemb number of memb
- * @param  void *userdata user defined data structurea
- * @return size of header data received.
- * @details this routine will be invoked multiple times by the libcurl until the full
- * header data are received.  we are only interested in the ECE252_HEADER line
- * received so that we can extract the image sequence number from it. This
- * explains the if block in the code.
- */
 size_t header_cb_curl(char *p_recv, size_t size, size_t nmemb, void *userdata)
 {
     int realsize = size * nmemb;
-    recv_chunk *p = userdata;
+    RECV_BUF *p = userdata;
 
     if (realsize > strlen(ECE252_HEADER) &&
 	strncmp(p_recv, ECE252_HEADER, strlen(ECE252_HEADER)) == 0) {
 
         /* extract img sequence number */
-	p->seq = atoi(p_recv + strlen(ECE252_HEADER));
+	    p->seq = atoi(p_recv + strlen(ECE252_HEADER));
 
     }
     return realsize;
 }
 
-
-/**
- * @brief write callback function to save a copy of received data in RAM.
- *        The received libcurl data are pointed by p_recv,
- *        which is provided by libcurl and is not user allocated memory.
- *        The user allocated memory is at p_userdata. One needs to
- *        cast it to the proper struct to make good use of it.
- *        This function maybe invoked more than once by one invokation of
- *        curl_easy_perform().
- */
-
-size_t write_cb_curl(char *p_recv, size_t size, size_t nmemb, void *p_userdata)
+size_t write_cb_curl3(char *p_recv, size_t size, size_t nmemb, void *p_userdata)
 {
     size_t realsize = size * nmemb;
-    recv_chunk *p = (recv_chunk *)p_userdata;
+    RECV_BUF *p = (RECV_BUF *)p_userdata;
 
     if (p->size + realsize + 1 > p->max_size) {/* hope this rarely happens */
-        fprintf(stderr, "User buffer is too small, abort...\n");
-        abort();
+        /* received data is not 0 terminated, add one byte for terminating 0 */
+        size_t new_size = p->max_size + max(BUF_INC, realsize + 1);
+        char *q = realloc(p->buf, new_size);
+        if (q == NULL) {
+            perror("realloc"); /* out of memory */
+            return -1;
+        }
+        p->buf = q;
+        p->max_size = new_size;
     }
 
     memcpy(p->buf + p->size, p_recv, realsize); /*copy data from libcurl*/
@@ -245,39 +176,27 @@ size_t write_cb_curl(char *p_recv, size_t size, size_t nmemb, void *p_userdata)
     return realsize;
 }
 
-/**
- * @brief calculate the actual size of RECV_BUF
- * @param size_t nbytes number of bytes that buf in RECV_BUF struct would hold
- * @return the REDV_BUF member fileds size plus the RECV_BUF buf data size
- */
-int sizeof_shm_recv_buf(size_t nbytes)
+int recv_buf_init(RECV_BUF *ptr, size_t max_size)
 {
-    return (sizeof(RECV_BUF) + sizeof(char) * nbytes);
-}
+    void *p = NULL;
 
-/**
- * @brief initialize the RECV_BUF structure.
- * @param RECV_BUF *ptr memory allocated by user to hold RECV_BUF struct
- * @param size_t nbytes the RECV_BUF buf data size in bytes
- * NOTE: caller should call sizeof_shm_recv_buf first and then allocate memory.
- *       caller is also responsible for releasing the memory.
- */
-
-int shm_recv_buf_init(RECV_BUF *ptr, size_t nbytes)
-{
-    if ( ptr == NULL ) {
+    if (ptr == NULL) {
         return 1;
     }
 
-    ptr->buf = (char *)ptr + sizeof(RECV_BUF);
-    ptr->size = 0;
-    ptr->max_size = nbytes;
-    ptr->seq = -1;              /* valid seq should be non-negative */
+    p = malloc(max_size);
+    if (p == NULL) {
+	    return 2;
+    }
 
+    ptr->buf = p;
+    ptr->size = 0;
+    ptr->max_size = max_size;
+    ptr->seq = -1;              /* valid seq should be non-negative */
     return 0;
 }
 
-int recv_buf_cleanup(recv_chunk *ptr)
+int recv_buf_cleanup(RECV_BUF *ptr)
 {
     if (ptr == NULL) {
 	return 1;
@@ -290,43 +209,76 @@ int recv_buf_cleanup(recv_chunk *ptr)
 }
 
 
-/**
- * @brief output data in memory to a file
- * @param path const char *, output file path
- * @param in  void *, input data to be written to the file
- * @param len size_t, length of the input data in bytes
- */
+void producer(queue* q, g_vars* g) {
+    CURL *curl_handle;
+    CURLcode res;
+    char url[256];
+    RECV_BUF recv_buf;
 
-int write_file(const char *path, const void *in, size_t len)
-{
-    FILE *fp = NULL;
+	while(1){
+		pthread_mutex_lock(&(g -> count_mutex));
 
-    if (path == NULL) {
-        fprintf(stderr, "write_file: file name is null!\n");
-        return -1;
+		int image_part = g -> num_pics;
+
+		if (image_part == 50) {
+			pthread_mutex_unlock(&(g -> count_mutex));
+			break;
+		}
+		(g -> num_pics)++;
+
+		pthread_mutex_unlock(&(g -> count_mutex));
+
+        sprintf(url, "http://ece252-%d.uwaterloo.ca:2530/image?img=%d&part=%d", image_part % 3 + 1, g -> image_num, image_part);
+        recv_buf_init(&recv_buf, BUF_SIZE);
+        curl_global_init(CURL_GLOBAL_DEFAULT);
+
+        /* init a curl session */
+        curl_handle = curl_easy_init();
+
+        if (curl_handle == NULL) {
+            fprintf(stderr, "curl_easy_init: returned NULL\n");
+        }
+
+        /* specify URL to get */
+        curl_easy_setopt(curl_handle, CURLOPT_URL, url);
+
+        /* register write call back function to process received data */
+        curl_easy_setopt(curl_handle, CURLOPT_WRITEFUNCTION, write_cb_curl3);
+        /* user defined data structure passed to the call back function */
+        curl_easy_setopt(curl_handle, CURLOPT_WRITEDATA, (void *)&recv_buf);
+
+        /* register header call back function to process received header data */
+        curl_easy_setopt(curl_handle, CURLOPT_HEADERFUNCTION, header_cb_curl);
+        /* user defined data structure passed to the call back function */
+        curl_easy_setopt(curl_handle, CURLOPT_HEADERDATA, (void *)&recv_buf);
+
+        /* some servers requires a user-agent field */
+        curl_easy_setopt(curl_handle, CURLOPT_USERAGENT, "libcurl-agent/1.0");
+
+
+        /* get it! */
+        res = curl_easy_perform(curl_handle);
+
+        if( res != CURLE_OK) {
+            fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
+        }
+
+        printf("image num: %d \n", recv_buf.seq);
+
+        /* cleaning up */
+        curl_easy_cleanup(curl_handle);
+        curl_global_cleanup();
+        recv_buf_cleanup(&recv_buf);
     }
-
-    if (in == NULL) {
-        fprintf(stderr, "write_file: input data is null!\n");
-        return -1;
-    }
-
-    fp = fopen(path, "wb");
-    if (fp == NULL) {
-        perror("fopen");
-        return -2;
-    }
-
-    if (fwrite(in, 1, len, fp) != len) {
-        fprintf(stderr, "write_file: imcomplete write!\n");
-        return -3;
-    }
-    return fclose(fp);
 }
 
+int write_file(int seq, const void *in, size_t len, int* count)
+{
+    return 0;
+}
 
 int main( int argc, char** argv )
-{	
+{
 	/*
     int buf_size = atoi(argv[1]);
 	int num_prod = atoi(argv[2]);
@@ -338,34 +290,34 @@ int main( int argc, char** argv )
 	int num_prod = 5;
 	int num_con = 5;
 	int sleep_time = 5;
-	int num_image = 5;
-	
+	int image_num = 1;
+
     int queue_id = shmget(IPC_PRIVATE, sizeof(queue) + sizeof(recv_chunk)*buf_size, IPC_CREAT | IPC_EXCL | S_IRUSR | S_IWUSR);
     void* temp_pointer = shmat(queue_id, NULL, 0);
     queue* queue_pointer = (queue*) temp_pointer;
     queue_init(queue_pointer, temp_pointer, buf_size);
-	
+
 	int global_id = shmget(IPC_PRIVATE, sizeof(g_vars), IPC_CREAT | IPC_EXCL | S_IRUSR | S_IWUSR);
 	void* global_temp = shmat(global_id, NULL, 0);
 	g_vars* var_pointer = (g_vars*) global_temp;
-	global_init(var_pointer);
-	
+	global_init(var_pointer, image_num);
+
 	/*
 	for (int i = 0; i < queue_pointer -> max_size; i++) {
 		recv_chunk* temp = queue_pointer -> buf + i;
 		printf("seq: %d, size: %d \n", temp -> seq, temp -> size);
 	}
-	*/ 
-	
+	*/
+
 	pid_t main_pid;
 	pid_t cpids[num_con + num_prod];
 	int state;
-	
+
 	// consumers
 	for( int i = 0; i < num_con; i++) {
-		
+
 		main_pid = fork();
-		
+
 		if ( main_pid > 0 ) {
 			cpids[i] = main_pid;
 		}
@@ -377,16 +329,17 @@ int main( int argc, char** argv )
 			exit(3);
 		}
 	}
-	
+
 	// producers
 	for( int i = num_con; i < num_con + num_prod; i++ ) {
-		
+
 		main_pid = fork();
-		
+
 		if ( main_pid > 0 ) {
 			cpids[i] = main_pid;
 		}
-		else if ( main_pid == 0 ){
+		else if ( main_pid == 0 ) {
+            producer(queue_pointer, var_pointer);
 			return 0;
 		}
 		else {
@@ -394,7 +347,7 @@ int main( int argc, char** argv )
 			exit(3);
 		}
 	}
-	
+
 	// parent
 	if ( main_pid > 0 ) {
 		for ( int i = 0; i < num_con + num_prod; i++ ) {
@@ -404,6 +357,6 @@ int main( int argc, char** argv )
             }
 		}
 	}
-	
+
 	return 0;
 }
