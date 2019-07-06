c45e2e3698892bec4988c7b6d283c03ab11919d3
chandlerlei2017 <chandlerlei87@gmail.com>
2019-07-06 13:38:29 -0400

    lab4 template

---
+++ lab4/findpng2.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <curl/curl.h>
+#include <libxml/HTMLparser.h>
+#include <libxml/parser.h>
+#include <libxml/xpath.h>
+#include <libxml/uri.h>
+
+#define SEED_URL "http://ece252-1.uwaterloo.ca/lab4/"
+#define ECE252_HEADER "X-Ece252-Fragment: "
+#define BUF_SIZE 1048576  /* 1024*1024 = 1M */
+#define BUF_INC  524288   /* 1024*512  = 0.5M */
+
+#define CT_PNG  "image/png"
+#define CT_HTML "text/html"
+#define CT_PNG_LEN  9
+#define CT_HTML_LEN 9
+
+#define max(a, b) \
+  ({ __typeof__ (a) _a = (a); \
+      __typeof__ (b) _b = (b); \
+    _a > _b ? _a : _b; })
+
+typedef struct recv_buf2 {
+  char *buf;       /* memory to hold a copy of received data */
+  size_t size;     /* size of valid data in buf in bytes*/
+  size_t max_size; /* max capacity of buf in bytes*/
+  int seq;         /* >=0 sequence number extracted from http header */
+                    /* <0 indicates an invalid seq number */
+} RECV_BUF;
+
+int main( int argc, char** argv )
+{
+  return 0;
+}
