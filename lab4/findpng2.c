99709a2ae4cba8d473653edcc4acaf30306c09b2
chandlerlei2017 <chandlerlei87@gmail.com>
2019-07-06 15:07:37 -0400

    linked list

--- lab4/findpng2.c
+++ lab4/findpng2.c
@@ -8,6 +8,8 @@
 #include <libxml/parser.h>
 #include <libxml/xpath.h>
 #include <libxml/uri.h>
+#include <search.h>
+#include "linked_list.h"
 
 #define SEED_URL "http://ece252-1.uwaterloo.ca/lab4/"
 #define ECE252_HEADER "X-Ece252-Fragment: "
@@ -32,7 +34,44 @@ typedef struct recv_buf2 {
                     /* <0 indicates an invalid seq number */
 } RECV_BUF;
 
+
+
+// Thread function to process the urls
+void *process_url(void *arg) {
+
+}
+
 int main( int argc, char** argv )
 {
+  int i;
+
+  linked_list* ll = malloc(sizeof(linked_list));
+  init(ll);
+
+  for (i = 0; i < 10; i++){
+    char element[256];
+    sprintf(element, "%d", i);
+
+    insert(ll, element);
+  }
+
+  for (i = 0; i < 10; i++) {
+    char* element = pop(ll);
+
+    printf("%s\n", element);
+
+    free(element);
+  }
+
+
+
+  free(ll);
+  // Initialize the hashset
+  hcreate(1000);
+
+
+
+  // destroy the hashset
+  hdestroy();
   return 0;
 }
---
+++ lab4/linked_list.h
@@ -0,0 +1,58 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+typedef struct node {
+  char buffer[256];
+  struct node* next;
+} node;
+
+typedef struct linked_list {
+  node* head;
+} linked_list;
+
+void init(linked_list* ll) {
+  ll -> head = NULL;
+}
+
+void insert(linked_list* ll, char* buffer) {
+  if (ll -> head == NULL) {
+    ll -> head = (node*) malloc(sizeof(node));
+    memcpy(ll -> head -> buffer, buffer, strlen(buffer));
+    ll -> head -> next = NULL;
+  }
+  else {
+    node* temp = (node*) malloc(sizeof(node));
+
+    memcpy(temp -> buffer, buffer, strlen(buffer));
+    temp -> next = ll -> head;
+    ll -> head = temp;
+  }
+}
+
+char* pop(linked_list* ll) {
+  if(ll -> head == NULL) {
+    printf("List size is zero, couldn't pop");
+  }
+  else {
+    node* temp = ll -> head;
+
+    char* ret_val = malloc(strlen(temp -> buffer));
+    memcpy(ret_val, temp -> buffer, strlen(temp -> buffer));
+    ll -> head = temp -> next;
+
+    free(temp);
+    return ret_val;
+  }
+}
+
+void cleanup(linked_list* ll) {
+  node* it = ll -> head;
+  while(it != NULL) {
+    node* temp = it;
+    it = it -> next;
+    free(temp);
+  }
+  ll -> head = NULL;
+}
+
